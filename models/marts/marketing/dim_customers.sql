52344dcf72e474205f24fc6360b38ae2464bfec7
mariano.garcia@dbtlabs.com <mariano.garcia@dbtlabs.com>
2025-08-07 15:02:55 +0000

    refactores dim_customers to add two staging models. Set project configs to materialize all marts as tables and staging as views

---
+++ models/marts/marketing/dim_customers.sql
@@ -0,0 +1,20 @@
+{{
+
+    config(
+        materialized='table'
+    )
+}}
+
+with customers as (
+
+    select *
+    from {{ ref('stg_jaffle_shop__customers') }}
+
+
+),
+
+orders as (
+
+    select *
+    from {{ ref('stg_jaffle_shop__orders') }}
+)
---
+++ models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
@@ -0,0 +1,7 @@
+select
+    id as customer_id,
+    user_id as customer_id,
+    order_date,
+    status
+
+from raw.jaffle_shop.orders
